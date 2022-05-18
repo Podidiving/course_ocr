9739dfbdeadbe6231ed58d5ec43da4988315335d
Veselyev Aleksandr <Veselev.aleks@gmail.com>
2022-05-18 12:54:35 +0300

    refactor

--- task2/src/arcface/focal.py
+++ task2/src/arcface/focal.py
@@ -26,7 +26,7 @@ class FocalLoss(nn.Module):
             )  # N,H*W,C => N*H*W,C
         target = target.view(-1, 1)
 
-        logpt = F.log_softmax(input)
+        logpt = F.log_softmax(input, -1)
         logpt = logpt.gather(1, target)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
